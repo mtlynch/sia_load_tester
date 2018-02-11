290e6ca987afbdae4275275e3fed8fba6a47d332
Michael Lynch <git@mtlynch.io>
2018-02-10 20:52:46 -0500

    Fixing spacing in log messages

--- sia_load_tester/main.py
+++ sia_load_tester/main.py
@@ -15,7 +15,7 @@ def configure_logging():
     root_logger = logging.getLogger()
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
-        '%(asctime)s %(name)-15s %(levelname)-4s %(message)s',
+        '%(asctime)s %(name)-16s %(levelname)-4s %(message)s',
         '%Y-%m-%d %H:%M:%S')
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
