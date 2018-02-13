466e1a6695168d9e768cc2475e0ee0ca110404d7
Michael Lynch <git@mtlynch.io>
2018-02-13 15:35:47 -0500

    Fixing Mbps calculation
    Need to divide by 1M

--- sia_load_tester/progress.py
+++ sia_load_tester/progress.py
@@ -118,9 +118,8 @@ class Tracker(object):
                 bytes_uploaded, self._get_upload_mbps_in_time_window())
             return bytes_uploaded
         else:
-            logger.info(
-                '%d bytes uploaded since tracking began (averaging %.2f Mbps)',
-                bytes_uploaded, self._get_upload_mbps_in_time_window())
+            logger.info('%d bytes uploaded since tracking began (averaging %.2f Mbps)',
+                        bytes_uploaded, self._get_upload_mbps_in_time_window())
             return None
 
     def _get_upload_mbps_in_time_window(self):
@@ -129,7 +128,8 @@ class Tracker(object):
         bytes_uploaded = self._window_bytes()
         time_window_seconds = (self._window_end_timestamp() -
                                self._window_start_timestamp()).total_seconds()
-        return (bytes_uploaded * 8.0) / time_window_seconds
+        megabits_uploaded =(bytes_uploaded * 8.0) / pow(10, 6)
+        return megabits_uploaded / time_window_seconds
 
     def _record_latest(self):
         self._progress_history.append(
