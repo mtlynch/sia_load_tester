87c3f381ff610179418334fbea183f6f8efdf4a3
Michael Lynch <git@mtlynch.io>
2018-02-13 11:33:52 -0500

    Adding an exit event to DatasetUploader and Waiter

--- sia_load_tester/sia_conditions.py
+++ sia_load_tester/sia_conditions.py
@@ -9,29 +9,45 @@ _SLEEP_SECONDS = 15
 logger = logging.getLogger(__name__)
 
 
-def make_waiter(upload_queue):
+class Error(Exception):
+    pass
+
+
+class WaitInterruptedError(Error):
+    pass
+
+
+def make_waiter(exit_event):
     """Factory for creating a Waiter using production settings."""
-    return Waiter(upload_queue, sc.make_sia_client(), time.sleep)
+    return Waiter(sc.make_sia_client(), time.sleep, exit_event)
 
 
 class Waiter(object):
     """Waits for conditions in Sia node to become true."""
 
-    def __init__(self, sia_client, sleep_fn):
+    def __init__(self, sia_client, sleep_fn, exit_event):
         """Creates a new Waiter instance.
 
         Args:
             sia_client: An implementation of the Sia client API.
             sleep_fn: A callback function for putting the thread to sleep for
                 a given number of seconds.
+            exit_event: An event that, when set, indicates Waiter should stop
+                waiting and raise an exception.
         """
         self._sia_client = sia_client
         self._sleep_fn = sleep_fn
+        self._exit_event = exit_event
 
     def wait_for_available_upload_slot(self):
+        """Waits until the number of concurrent uploads is below the maximum.
+
+        Raises:
+            WaitInterruptedError if the exit event is set during function
+                execution.
+        """
         upload_count = self._count_uploads_in_progress()
         while self._too_many_uploads_in_progress(upload_count):
-
             logger.info(('Too many uploads in progress: %d >= %d.'
                          ' Sleeping for %d seconds'), upload_count,
                         _MAX_CONCURRENT_UPLOADS, _SLEEP_SECONDS)
@@ -39,6 +55,12 @@ class Waiter(object):
             upload_count = self._count_uploads_in_progress()
 
     def wait_for_all_uploads_to_complete(self):
+        """Waits until all in-progress uploads are complete.
+
+        Raises:
+            WaitInterruptedError if the exit event is set during function
+                execution.
+        """
         upload_count = self._count_uploads_in_progress()
         while upload_count > 0:
             logger.info(
@@ -49,6 +71,7 @@ class Waiter(object):
             upload_count = self._count_uploads_in_progress()
 
     def _count_uploads_in_progress(self):
+        self._check_exit_event()
         n = 0
         for sia_file in self._sia_client.renter_files():
             if sia_file[u'uploadprogress'] < 100:
@@ -57,3 +80,9 @@ class Waiter(object):
 
     def _too_many_uploads_in_progress(self, concurrent_uploads):
         return concurrent_uploads >= _MAX_CONCURRENT_UPLOADS
+
+    def _check_exit_event(self):
+        if self._exit_event.is_set():
+            logger.critical('Exit event is set. Stopping wait.')
+            raise WaitInterruptedError(
+                'Sia condition wait has been interrupted')
--- tests/test_sia_conditions.py
+++ tests/test_sia_conditions.py
@@ -1,3 +1,4 @@
+import threading
 import unittest
 
 import mock
@@ -12,8 +13,9 @@ class WaiterTest(unittest.TestCase):
         self.mock_files = []
         self.mock_sia_client = mock.Mock()
         self.mock_sia_client.renter_files.side_effect = lambda: self.mock_files
+        self.exit_event = threading.Event()
         self.waiter = sia_conditions.Waiter(self.mock_sia_client,
-                                            self.mock_sleep_fn)
+                                            self.mock_sleep_fn, self.exit_event)
 
     def increment_upload_progress_by_one(self):
         """Simulates all files making +1% upload progress."""
@@ -54,6 +56,40 @@ class WaiterTest(unittest.TestCase):
 
         self.assertEqual(100 - 94, self.mock_sleep_fn.call_count)
 
+    def test_wait_for_available_upload_slot_raises_exception_when_exit_event_is_set_before_call(
+            self):
+        self.mock_files = []
+        self.exit_event.set()
+
+        with self.assertRaises(sia_conditions.WaitInterruptedError):
+            self.waiter.wait_for_available_upload_slot()
+
+    def test_wait_for_available_upload_slot_raises_exception_when_exit_event_is_set_after_first_sleep(
+            self):
+        self.mock_files = [
+            {
+                u'uploadprogress': 90,
+            },
+            {
+                u'uploadprogress': 91,
+            },
+            {
+                u'uploadprogress': 92,
+            },
+            {
+                u'uploadprogress': 93,
+            },
+            {
+                u'uploadprogress': 94,
+            },
+        ]
+        self.mock_sleep_fn.side_effect = lambda _: self.exit_event.set()
+
+        with self.assertRaises(sia_conditions.WaitInterruptedError):
+            self.waiter.wait_for_available_upload_slot()
+
+        self.assertEqual(1, self.mock_sleep_fn.call_count)
+
     def test_wait_for_all_uploads_to_complete_returns_immediately_when_zero_uploads_are_in_progress(
             self):
         self.mock_files = []
@@ -86,3 +122,53 @@ class WaiterTest(unittest.TestCase):
         self.waiter.wait_for_all_uploads_to_complete()
 
         self.assertEqual(100 - 90, self.mock_sleep_fn.call_count)
+
+    def test_wait_for_all_uploads_raises_exception_when_exit_event_is_set_before_call(
+            self):
+        self.mock_files = [
+            {
+                u'uploadprogress': 90,
+            },
+            {
+                u'uploadprogress': 91,
+            },
+            {
+                u'uploadprogress': 92,
+            },
+            {
+                u'uploadprogress': 93,
+            },
+            {
+                u'uploadprogress': 94,
+            },
+        ]
+        self.exit_event.set()
+
+        with self.assertRaises(sia_conditions.WaitInterruptedError):
+            self.waiter.wait_for_all_uploads_to_complete()
+
+    def test_wait_for_all_uploads_raises_exception_when_exit_event_is_set_after_first_sleep(
+            self):
+        self.mock_files = [
+            {
+                u'uploadprogress': 90,
+            },
+            {
+                u'uploadprogress': 91,
+            },
+            {
+                u'uploadprogress': 92,
+            },
+            {
+                u'uploadprogress': 93,
+            },
+            {
+                u'uploadprogress': 94,
+            },
+        ]
+        self.mock_sleep_fn.side_effect = lambda _: self.exit_event.set()
+
+        with self.assertRaises(sia_conditions.WaitInterruptedError):
+            self.waiter.wait_for_all_uploads_to_complete()
+
+        self.assertEqual(1, self.mock_sleep_fn.call_count)
