617ba6f8f8f72580094e78f2e82e4e281b446448
Michael Lynch <git@mtlynch.io>
2018-02-12 23:22:57 -0500

    Adding logic to dump state at beginning and end of test

--- sia_load_tester/main.py
+++ sia_load_tester/main.py
@@ -1,40 +1,57 @@
 #!/usr/bin/python2
 
 import argparse
+import os
 import logging
 
 import contracts
 import dataset
 import dataset_uploader
 import preconditions
+import state
 import upload_queue
 
 logger = logging.getLogger(__name__)
 
 
-def configure_logging():
+def configure_logging(output_dir):
     root_logger = logging.getLogger()
-    handler = logging.StreamHandler()
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(
+        os.path.join(output_dir, 'sia_load_test.log'))
     formatter = logging.Formatter(
         '%(asctime)s %(name)-16s %(levelname)-4s %(message)s',
         '%Y-%m-%d %H:%M:%S')
-    handler.setFormatter(formatter)
-    root_logger.addHandler(handler)
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(stream_handler)
+    root_logger.addHandler(file_handler)
     root_logger.setLevel(logging.INFO)
 
 
 def main(args):
-    configure_logging()
+    _ensure_directory_exists(args.output_dir)
+    configure_logging(args.output_dir)
     logger.info('Started runnning')
+
     preconditions.check()
+
+    snapshotter = state.make_snapshotter(args.output_dir)
+    snapshotter.snapshot()
+
     contracts.ensure_min_contracts()
     input_dataset = dataset.load_from_path(args.dataset_root)
     queue = upload_queue.from_dataset(input_dataset)
-    # TODO(mtlynch): Create upload queue.
     uploader = dataset_uploader.make_dataset_uploader(queue)
     uploader.upload()
-    # TODO(mtlynch): Dump state.
+
+    snapshotter.snapshot()
     logger.info('Test completed successfully')
+
+
+def _ensure_directory_exists(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
 
 if __name__ == '__main__':
@@ -46,4 +63,9 @@ if __name__ == '__main__':
         '--dataset_root',
         required=True,
         help='Path to root directory of data to upload to Sia')
+    parser.add_argument(
+        '-o',
+        '--output_dir',
+        required=True,
+        help='Path to directory for test output files')
     main(parser.parse_args())
---
+++ sia_load_tester/state.py
@@ -0,0 +1,73 @@
+import datetime
+import json
+import logging
+import os
+
+import pysia
+
+logger = logging.getLogger(__name__)
+
+
+def make_snapshotter(output_dir):
+    return Snapshotter(output_dir, pysia.Sia(), datetime.datetime.utcnow)
+
+
+class Snapshotter(object):
+
+    def __init__(self, output_dir, sia_api, time_fn):
+        self._output_dir = output_dir
+        self._sia_api = sia_api
+        self._time_fn = time_fn
+
+    def snapshot(self):
+        _ensure_directory_exists(self._output_dir)
+        snapshot_fns = [
+            self._snapshot_renter, self._snapshot_contract,
+            self._snapshot_renter_prices, self._snapshot_renter_files,
+            self._snapshot_wallet
+        ]
+        timestamp = self._time_fn()
+        for snapshot_fn in snapshot_fns:
+            try:
+                snapshot_fn(timestamp)
+            except Exception as e:
+                logger.error(
+                    'Error when attempting to call snapshot function %s: %s',
+                    snapshot_fn.__name__, e.message)
+
+    def _snapshot_renter(self, timestamp):
+        _snapshot_api(self._sia_api.get_renter, self._output_dir, 'renter',
+                      timestamp)
+
+    def _snapshot_contract(self, timestamp):
+        _snapshot_api(self._sia_api.get_renter_contracts, self._output_dir,
+                      'contracts', timestamp)
+
+    def _snapshot_renter_prices(self, timestamp):
+        _snapshot_api(self._sia_api.get_renter_prices, self._output_dir,
+                      'prices', timestamp)
+
+    def _snapshot_renter_files(self, timestamp):
+        _snapshot_api(self._sia_api.get_renter_files, self._output_dir, 'files',
+                      timestamp)
+
+    def _snapshot_wallet(self, timestamp):
+        _snapshot_api(self._sia_api.get_wallet, self._output_dir, 'wallet',
+                      timestamp)
+
+
+def _ensure_directory_exists(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+
+def _snapshot_api(sia_api_fn, output_dir, snapshot_name, timestamp):
+    output_path = _make_output_path(output_dir, snapshot_name, timestamp)
+    logger.info('Snapshotting %s state to %s', snapshot_name, output_path)
+    with open(output_path, 'w') as output_file:
+        json.dump(sia_api_fn(), output_file)
+
+
+def _make_output_path(output_dir, snapshot_name, timestamp):
+    return os.path.join(output_dir, '%s-%s.json' %
+                        (timestamp.strftime('%Y-%m-%dT%H%M%SZ'), snapshot_name))
